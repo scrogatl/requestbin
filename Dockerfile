02483d51a8cc63b30ae35549e1d8204ae92d53d2
Scott Rogers <scottrogers@newrelic.com>
2020-02-07 18:41:14 +0000

    Updates to added Fluent logging and New Relic Ini

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,18 @@ RUN apk update && apk upgrade && \
         bsd-compat-headers \
         # event.h
         libevent-dev \
+                ruby \
+        ruby-dev \
+        make   \
     && rm -rf /var/cache/apk/*
+
+RUN     gem install webrick
+RUN     gem install json
+RUN     gem install etc
+RUN     gem install fluentd
+RUN     fluent-gem install fluent-plugin-newrelic
+RUN     fluentd --setup ./fluent
+    
 
 # want all dependencies first so that if it's just a code change, don't have to
 # rebuild as much of the container
@@ -22,7 +33,8 @@ ADD requestbin  /opt/requestbin/requestbin/
 
 EXPOSE 8000
 
-WORKDIR /opt/requestbin
-CMD gunicorn -b 0.0.0.0:8000 --worker-class gevent --workers 2 --max-requests 1000 requestbin:app
+#WORKDIR /opt/requestbin
+#CMD gunicorn -b 0.0.0.0:8000 --worker-class gevent --workers 2 --max-requests 1000 requestbin:app
 
-
+ENV NEW_RELIC_CONFIG_FILE=/opt/requestbin/requestbin/newrelic.ini
+CMD newrelic-admin run-program gunicorn -b 0.0.0.0:8000 --worker-class gevent --workers 2 --max-requests 1000 requestbin:app
--- requestbin/api.py
+++ requestbin/api.py
@@ -4,6 +4,22 @@ import operator
 from flask import session, make_response, request, render_template
 from requestbin import app, db
 
+# Import the logging module and the New Relic log formatter
+import logging
+from newrelic.agent import NewRelicContextFormatter
+
+# Instantiate a new log handler
+# handler = logging.StreamHandler()
+handler = logging.FileHandler("requestbin.log")
+
+# Instantiate the log formatter and add it to the log handler
+formatter = NewRelicContextFormatter()
+handler.setFormatter(formatter)
+
+# Get the root logger and add the handler to it
+root_logger = logging.getLogger("requestbin")
+root_logger.addHandler(handler)
+
 def _response(object, code=200):
     jsonp = request.args.get('jsonp')
     if jsonp:
@@ -19,7 +35,10 @@ def _response(object, code=200):
 @app.endpoint('api.bins')
 def bins():
     private = request.form.get('private') in ['true', 'on']
+    
     bin = db.create_bin(private)
+    root_logger.warn("create bin: " + bin.name) 
+    print(  "create bin: " + bin.name)
     if bin.private:
         session[bin.name] = bin.secret_key
     return _response(bin.to_dict())
